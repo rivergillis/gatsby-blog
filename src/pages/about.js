7f96979d7b3aa6be26b4a04199bed1eced32c379
River Gillis <jrivergillis@gmail.com>
2019-05-01 22:24:45 -0500

    About the author

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,13 +9,19 @@ export default () => (
     <h1>About me</h1>
     <p>
       Hey there, it looks like you’ve found my blog! I’m a software engineer
-      with experience in graphics and systems engineering. Right now I’m
-      graduating from the University of Arkansas. Pretty soon I'll be working on
-      something fun at Google. Here’s a picture of me so that you know that I am
-      a real person:
+      with experience in systems engineering, graphics, and full-stack web
+      development. Right now I’m graduating from the University of Arkansas.
+      Pretty soon I'll be working on something fun at Google. Here’s a picture
+      of me so that you know that I am a real person:
       <img
         src="/assets/meirl.jpg"
         alt="River's face with a camera obscuring it"
+        css={css`
+          display: block;
+          margin: auto;
+          padding-top: 1em;
+          width: 70%;
+        `}
       />
     </p>
     <p>Close enough.</p>
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -46,6 +46,25 @@ export default function Template({
           />
         </div>
       </div>
+      <hr />
+      <div id="about-author">
+        <img
+          src="/assets/meirl.jpg"
+          alt="River's face"
+          css={css`
+            float: left;
+            margin-right: 15px;
+            width: 180px;
+          `}
+        />
+        <b>River Gillis</b> is a software engineer with experience in systems
+        engineering, graphics, and full-stack web development. Right now he is
+        finishing up his degree before heading to work at Google. You can find
+        him on <a href="https://twitter.com/rivergillis">Twitter</a>,{" "}
+        <a href="https://github.com/rivergillis">GitHub</a>,{" "}
+        <a href="https://www.instagram.com/therivergillis/">Instagram</a>, and
+        probably other places if you look hard enough.
+      </div>
     </Layout>
   )
 }
